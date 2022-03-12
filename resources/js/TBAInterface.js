812f319ed5e5f5603a1bbd3e246c3a6632702b79
ecjiang016 <95730592+ecjiang016@users.noreply.github.com>
2022-03-12 14:13:25 -0600

    Added API token

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "pd969sApngnHQTvGRYN25RLbrvwjnB4kRZSVaA7mDaSl0G1h5U3JqfPTuhN2pCWE";
 
 /**
  * Get list of teams in event
